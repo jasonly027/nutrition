23460a83c2ad43565893c47e5f5073ed76fc3e9e
Alex Situ <situalex123@gmail.com>
2024-11-01 10:55:06 -0700

    tailwind

---
+++ src/Login.tsx
@@ -0,0 +1,10 @@
+// MyComponent.js
+import React from 'react';
+
+const LoginComponent = () => {
+  return (
+    <div className='text-white'>Login </div>
+  );
+};
+
+export default LoginComponent;
